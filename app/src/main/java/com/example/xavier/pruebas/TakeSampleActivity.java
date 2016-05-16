0fd058193aa52c1e8ab8e265148e434d36ff295c
javier7ar <javier7ar@gmail.com>
2016-05-16 18:35:02 -0300

    i got it !

--- app/src/main/java/com/example/xavier/pruebas/TakeSampleActivity.java
+++ app/src/main/java/com/example/xavier/pruebas/TakeSampleActivity.java
@@ -1,29 +1,30 @@
 package com.example.xavier.pruebas;
 
+import android.app.Activity;
 import android.app.Fragment;
-import android.app.FragmentContainer;
+import android.app.FragmentTransaction;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class TakeSampleActivity extends AppCompatActivity implements PhotoFragment.OnPhotoFragmentInteractionListener,
+public class TakeSampleActivity extends Activity implements PhotoFragment.OnPhotoFragmentInteractionListener,
                                                                      InformationFragment.OnInformationFragmentInteractionListener{
 
     private List<Class> pasos;
     private int proximoPaso = 0;
+    private TextView lb_nro_paso;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_sample);
+
+        lb_nro_paso = (TextView) findViewById(R.id.lb_nro_paso);
 
         pasos = new ArrayList<Class>();
 
@@ -39,12 +40,18 @@ public class TakeSampleActivity extends AppCompatActivity implements PhotoFragme
 
     private void siguientePaso() {
         try {
+
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
             Fragment fragmento = (Fragment) pasos.get(proximoPaso).newInstance();
 
-            getFragmentManager().beginTransaction()
-                    .add(R.id.container, fragmento).commit();
+            transaction.replace(R.id.container, fragmento);
+            transaction.addToBackStack(null);
+            Log.e("TakeSampleActivity", "addToBackStack");
+            transaction.commit();
 
             proximoPaso++;
+
+            lb_nro_paso.setText(String.valueOf(proximoPaso)+"/"+String.valueOf(pasos.size()));
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
